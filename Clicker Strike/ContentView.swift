59473c1dcc0839d5971dc551756ad7d48dadfb4b
Lincoln Kermit <104798220+LincolnKermit@users.noreply.github.com>
2024-07-25 01:38:48 +0200

    Updated score multiplicator!

--- Clicker Strike/ContentView.swift
+++ Clicker Strike/ContentView.swift
@@ -1,18 +1,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isAnimating = false
     @State private var isClicked = false
-    @State public var score = 0
-    
+    @State private var score = 0
+    @State private var jetons = 1
     let seconds = 0.2
     
     var body: some View {
         VStack {
-            Text("Score: \(score)")
-                .font(.largeTitle)
+            Text("Hey, Let's click")
+                .font(.title)
                 .foregroundColor(.white)
                 .padding(.top, 40)
+            
+            Text("\(score)")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .padding(.top, 20)
             
             Spacer()
             
@@ -27,11 +31,26 @@ struct ContentView: View {
                     DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
                         isClicked.toggle()
                     }
-                    score += 1
-                    print("LOG: Score + 1 ")
+                    score += jetons
+                    jetons = score // score = 1^^x ( where x reprensent the click )
                 }
             
             Spacer()
+            
+            if score > 2^10 {
+                VStack {
+                    Text("Awesome!!! 🎉")
+                        .font(.title)
+                        .foregroundColor(.yellow)
+                    Text("X10")
+                        .font(.title)
+                        .foregroundColor(.red)
+                    Text("🎉🎉🎉")
+                        .font(.title)
+                        .bold()
+                }
+                .padding()
+            }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.black)
